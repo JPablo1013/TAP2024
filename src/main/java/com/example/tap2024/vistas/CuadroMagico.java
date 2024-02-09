26758ea5a06ac4722dc3359b72b9ab2242d13b7a
Juan Pablo Garcia Vega <18031402@itcelaya.edu.mx>
2024-02-08 18:47:08 -0600

    calculadora

--- src/main/java/com/example/tap2024/vistas/CuadroMagico.java
+++ src/main/java/com/example/tap2024/vistas/CuadroMagico.java
@@ -17,3 +17,9 @@ public class CuadroMagico extends Stage {
         scene = new Scene(new Button("da click aqui"));
     }
 }
+
+//git pull es para usar de manera cooperativa, toma los datos del compañero con mis compañeros
+//para guardar
+//git add .
+// git commit -a -m "comentario"
+//git push origin main
